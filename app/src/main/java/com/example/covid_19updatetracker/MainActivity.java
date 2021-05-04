54e9c2482e9be83fa48e22dd5239e614eae0bf01
Abdullah272056 <abdullah272056@gmail.com>
2021-05-04 14:29:53 +0600

    get all data

--- app/src/main/java/com/example/covid_19updatetracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19updatetracker/MainActivity.java
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.covid_19updatetracker.modelClass.OurObjectDataClass;
 import com.example.covid_19updatetracker.retrofit.ApiInterface;
 import com.example.covid_19updatetracker.retrofit.RetrofitClient;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -22,7 +24,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    //List<ResponseData> allDataList;
+    List<OurObjectDataClass> allDataList;
     List<String> nameList;
     String token;
     ApiInterface apiInterface;
@@ -40,18 +42,43 @@ public class MainActivity extends AppCompatActivity {
 
         apiInterface = RetrofitClient.getRetrofit("https://coronavirus-19-api.herokuapp.com/").create(ApiInterface.class);
 
+            apiInterface.getAllData().enqueue(new Callback<List<OurObjectDataClass>>() {
+                @Override
+                public void onResponse(Call<List<OurObjectDataClass>> call, Response<List<OurObjectDataClass>> response) {
+                    if (response.code()==200){
+                        allDataList=new ArrayList<>();
+                        Log.e("response","success");
+                        allDataList.addAll(response.body());
 
-        apiInterface.getData().enqueue(new Callback<OurObjectDataClass>() {
-            @Override
-            public void onResponse(Call<OurObjectDataClass> call, Response<OurObjectDataClass> response) {
-                Log.e("size","sss");
-            }
+                        Log.e("size",String.valueOf( allDataList.size()));
+                    }else {
+                        Toast.makeText(MainActivity.this, "Try again", Toast.LENGTH_SHORT).show();
+                    }
 
-            @Override
-            public void onFailure(Call<OurObjectDataClass> call, Throwable t) {
-                Log.e("size",t.getMessage());
-            }
-        });
+
+                }
+
+                @Override
+                public void onFailure(Call<List<OurObjectDataClass>> call, Throwable t) {
+                    Log.e("response","failed");
+
+                }
+            });
+
+
+//        apiInterface.getData().enqueue(new Callback<OurObjectDataClass>() {
+//            @Override
+//            public void onResponse(Call<OurObjectDataClass> call, Response<OurObjectDataClass> response) {
+//                Log.e("size","sss");
+//                Log.e("size",String.valueOf(response.body().getCountry()));
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<OurObjectDataClass> call, Throwable t) {
+//                Log.e("size",t.getMessage());
+//            }
+//        });
 
 
 
--- app/src/main/java/com/example/covid_19updatetracker/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/covid_19updatetracker/retrofit/ApiInterface.java
@@ -3,10 +3,15 @@ package com.example.covid_19updatetracker.retrofit;
 import com.example.covid_19updatetracker.modelClass.OurObjectDataClass;
 
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
     @GET("countries/afghanistan")
     Call<OurObjectDataClass> getData();
+
+    @GET("countries")
+    Call<List<OurObjectDataClass>> getAllData();
 }
